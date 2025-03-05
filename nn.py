8b2c12cccb5afb575c053e7a7c1e8c2eb86f8a7c
IIcaza <155965802+iic201@users.noreply.github.com>
2025-03-05 16:32:34 +0100

    main structure

---
+++ nn.py
@@ -0,0 +1,26 @@
+import numpy
+import pandas 
+import matplotlib 
+
+class NN:
+    def __init__(self, input_layer, hidden_layer, output_layer):
+
+        self.input_layer = input_layer
+        self.hidden_layer = hidden_layer
+        self.output_layer = output_layer
+        
+    def forward_prop(self, input_data):
+        pass
+
+    def ReLU(self, x):
+        pass
+
+    def backward_prop(self):
+        pass
+
+    def train(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
